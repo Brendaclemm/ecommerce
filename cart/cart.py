962f5e90fa67c8dc06214f2c3823a34b30d13e53
brendaAnyango <brendaclemm@gmail.com>
2024-10-09 15:49:18 +0300

    cart functionality complete

--- cart/cart.py
+++ cart/cart.py
@@ -15,14 +15,16 @@ class Cart:
         # make sure cart is available on all pages of site
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = quantity
 
         # logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = product_qty
 
         self.session.modified = True
 
@@ -38,3 +40,51 @@ class Cart:
 
         # return looked up products
         return products
+
+    def get_quantities(self):
+        quantities = self.cart
+        return quantities
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = quantity
+
+        # get cart
+        our_cart = self.cart
+        # update dictionary/cart
+        our_cart[product_id] = product_qty
+
+        self.session.modified = True
+
+        return self.cart
+
+    def delete_item(self, product):
+        product_id = str(product)
+
+        # delete from dictionary
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
+
+    def total(self):
+        # get product ids
+        product_ids = self.cart.keys()
+
+        # look up those keys in our product database
+        products = Product.objects.filter(id__in=product_ids)
+        quantities = self.cart
+
+        # start counting from 0
+        total = 0
+
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total += (product.sale_price * value)
+                    else:
+                        total += (product.price * value)
+
+        return total
--- cart/views.py
+++ cart/views.py
@@ -8,7 +8,13 @@ from django.http import JsonResponse
 def cart_summary(request):
     cart = Cart(request)
     cart_products = cart.get_prods()
-    return render(request, "cart/cart_summary.html", {"cart": cart.cart, 'cart_products': cart_products})
+    quantities = cart.get_quantities()
+    totals = cart.total()
+    return render(request, "cart/cart_summary.html",
+                  {"cart": cart.cart,
+                   'cart_products': cart_products,
+                   "quantities": quantities,
+                   'totals': totals})
 
 
 def cart_add(request):
@@ -19,11 +25,12 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         # get stuff
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         # lookup product in database
         product = get_object_or_404(Product, id=product_id)
 
         # save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
 
         # return response
         # response = JsonResponse({'Product Name: ': product.name})
@@ -39,8 +46,25 @@ def cart_add(request):
 
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        cart.delete_item(product=product_id)
+
+        return redirect('cart_summary')
 
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        # get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_quantity'))
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty': product_qty})
+        return response
+        # return redirect('cart_summary')
